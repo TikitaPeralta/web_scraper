d57a8b1339361016b422c857d37ed9945506522c
TIKITA PERALTA <tikitaperalta@gmail.com>
2023-12-20 11:53:45 +0000

    into func

--- scraping.py
+++ scraping.py
@@ -3,35 +3,49 @@ from bs4 import BeautifulSoup
 from random import randint 
 from time import sleep 
 
-URL = "https://www.wikihow.com/Main-Page/"
+def method1(URL):
+    req = requests.get(URL)
+    soup = BeautifulSoup(req.content, "html.parser")
+    results = soup.find_all("a")
 
-#for page in range(1, 3):
-req = requests.get(URL)
-#print(req.text)
-soup = BeautifulSoup(req.content, "html.parser")
-
-#method1: main page
-results = soup.find_all("a")
-
-for res in results:
-    #print(res, end="\n"*2)
-    res_descendants = res.descendants
-    for d in res_descendants:
-        #print('d: ', d, end='\n'*2)
-        if d.name == 'img' and d.get('src'):
-            print('img: ', 'https://www.wikihow.com'+d['src'])
-        if d.name == 'p':
-            print('alt: ', d.text)
+    for res in results:
+        res_descendants = res.descendants
+        for d in res_descendants:
+            if d.name == 'img' and d.get('src'):
+                img0 = d['src']
+                if img0[-3:-1] != 'png' or 'svg':
+                    print('img: ', 'https://www.wikihow.com'+ img0)
+                    img1 = 'https://www.wikihow.com'+ img0
+                    
+            elif d.name == 'p':
+                cap1 = d.text
+                print('alt: ', d.text)
+            return cap1, img1
         
     #print(res.get('href')) # => add to URL list
+                
+def method2(URL):
+    req = requests.get(URL)
+    soup = BeautifulSoup(req.content, "html.parser")
+    results1 = soup.find_all("img", alt=True, src=True)
 
-'''
-# method2: branch page
-results1 = soup.find_all("img", alt=True, src=True)
+    for res in results1:
+        cap2 = res['alt']
+        img2 = res['src']
+        print(img2[-4:-1])
+        if img2[-4:-1] != '.png' or '.svg':
+            print(cap2, img2, end="\n"*2)
+        return cap2, img2
 
-for res in results1:
-    print(res['alt'], res['src'], end="\n"*2)
-'''
+
+
+URL = "https://www.wikihow.com/Main-Page/"
+
+def scrape():
     
-#sleep(randint(2,10))
+
+
+
+
+    #sleep(randint(2,10))
 
