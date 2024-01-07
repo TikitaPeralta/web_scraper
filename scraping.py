6fbad706386b60bc30c71f3cbfd385f98b3b972e
Tikita Peralta <tikitaperalta@gmail.com>
2024-01-07 15:13:55 +0000

    progress main funct

--- scraping.py
+++ scraping.py
@@ -3,42 +3,71 @@ from bs4 import BeautifulSoup
 from random import randint 
 from time import sleep 
 
-URL = "https://www.wikihow.com/Main-Page/"
+def method1(URL):
+    req = requests.get(URL)
+    soup = BeautifulSoup(req.content, "html.parser")
+    results = soup.find_all("a")
 
-#for page in range(1, 3):
-req = requests.get(URL)
-#print(req.text)
-soup = BeautifulSoup(req.content, "html.parser")
+    for res in results:
+        l = res.get('href')
+        print(l)
+        res_descendants = res.descendants
+        for d in res_descendants:
+            if d.name == 'img' and d.get('src'):
+                img0 = d['src']
+                if img0[-3:-1] != 'png' or 'svg':
+                    print('img: ', 'https://www.wikihow.com'+ img0)
+                    img1 = 'https://www.wikihow.com'+ img0
+                    
+            elif d.name == 'p':
+                cap1 = d.text
+                print('alt: ', d.text)
+            return cap1, img1, l
 
-#method1: main page
-results = soup.find_all("a")
 
-for res in results:
-    #print(res, end="\n"*2)
-    res_descendants = res.descendants
-    for d in res_descendants:
-        #print('d: ', d, end='\n'*2)
-        if d.name == 'img' and d.get('src'):
-            print('img: ', 'https://www.wikihow.com'+d['src'])
-        if d.name == 'p':
-            print('alt: ', d.text)
-        
-    #print(res.get('href')) # => add to URL list
+def method2(URL):
+    req = requests.get(URL)
+    soup = BeautifulSoup(req.content, "html.parser")
+    results1 = soup.find_all("img", alt=True, src=True)
 
-'''
-# method2: branch page
-results1 = soup.find_all("img", alt=True, src=True)
-
-for res in results1:
-    print(res['alt'], res['src'], end="\n"*2)
-'''
+    for res in results1:
+        cap2 = res['alt']
+        img2 = res['src']
+        print(img2[-4:-1])
+        if img2[-4:-1] != '.png' or '.svg':
+            print(cap2, img2, end="\n"*2)
+        return cap2, img2
     
-#sleep(randint(2,10))
+def link2(URL):
+    req = requests.get(URL)
+    soup = BeautifulSoup(req.content, "html.parser")
+    results2 = soup.find_all("a", href=True, rel=True)
+    for r in results2:
+        l = r['href']
+        if l[0] == '/':
+            print('https://www.wikihow.com'+l)
+        return 'https://www.wikihow.com'+l
 
-req = requests.get("https://www.wikihow.com/Improve-Your-Personality")
-soup = BeautifulSoup(req.content, "html.parser")
-results2 = soup.find_all("a", href=True, rel=True)
-for r in results2:
-    if '#' not in r:
-        print(r['href'])
-    #print('https://www.wikihow.com'+r['href'])
+
+
+url = ["https://www.wikihow.com/Main-Page/"]
+done = []
+
+def scrape():
+    while len(url) != 0:
+        for u in url:
+            if u not in done:
+                done.append(u)
+                # main page method:
+                if u[-10:] != '/Main-Page/':
+                    caption1, picture1, link1 = method1(u)
+                    if link1 not in done:
+                        url.append(link1)
+                else:
+                    caption2, picture2 = method2(u)
+                    l = link2(u)
+                    if l not in done:
+                        url.append(l)
+
+
+    #sleep(randint(2,10))
