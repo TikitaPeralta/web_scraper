c9458b2a914ca62ee7a61c3a871e3d8b409bf403
TIKITA PERALTA <tikitaperalta@gmail.com>
2024-01-14 00:52:06 +0000

    sosmething

--- scraping.py
+++ scraping.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from random import randint 
 from time import sleep 
-
+'''
 def img1(URL):
     req = requests.get(URL)
     soup = BeautifulSoup(req.content, "html.parser")
@@ -31,6 +31,50 @@ def cap1(URL):
                 caption = d.text
                 print('alt: ', d.text)
                 return caption
+'''            
+def find_img_text_pairs(url):
+    req = requests.get(url)
+    soup = BeautifulSoup(req.content, "html.parser")
+    results = soup.find_all("a")
+    new_results = []
+    for res in results:
+        tags_in_descendents = []
+        if res.descendants is not None:
+            for d in res.descendants:
+                if d.name is not None:
+                    tags_in_descendents.append(d.name)
+        if "img" in tags_in_descendents and "p" in tags_in_descendents:
+            new_results.append(res)
+    two_results = []
+    for res in new_results:
+        for d in res.descendants:
+            if d.name == "img" and d.get('src'):
+                source = d['src']
+                if source[-4:] != '.png' and source[-4:] != '.svg': 
+                    two_results.append(res)
+                break
+    fin = []
+    for r in two_results:
+        #print(r)
+        link0 = r.get('href')
+        link1 = 'https://www.wikihow.com'+ link0
+        #print(link1) # imgs
+        for t in r.descendants:
+            s = soup.find("div", class_="content-spacer")
+            for i in s.descendants:
+                #print(i)
+                if i.name == 'img' and i.get('src'):
+                    print(i['src'])
+            if t.name == 'p':
+                fin.append([link1, t.text])
+                #print(t.text)
+            
+
+    #print(f"{len(results)} vs {len(new_results)} vs {len(two_results)}")
+
+    print(fin)
+    return fin
+
 
 def method2(URL):
     req = requests.get(URL)
@@ -79,5 +123,9 @@ def scrape():
                     #    url.append(l)
                     print('caption2: ', caption2, 'picture2: ', picture2)
 
-scrape()
+#scrape()
     #sleep(randint(2,10))
+
+URL = "https://www.wikihow.com/Main-Page/"
+pairs = find_img_text_pairs(URL)
+print(pairs)
